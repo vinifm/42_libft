c65f5c76ed732657cd58c45ba6c352f991eb8c54
Vinicius Ferreira <67122007+vinifm@users.noreply.github.com>
2021-08-27 16:56:14 -0300

    correct ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 13:50:26 by viferrei          #+#    #+#             */
-/*   Updated: 2021/08/25 09:19:03 by viferrei         ###   ########.fr       */
+/*   Updated: 2021/08/27 16:55:43 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 char	*ft_strchr(const char *str, int c)
 {
-	while (str != 0)
+	while (*str != 0)
 	{
-		if (*str == c)
+		if (*str == (char)c)
 			return ((char *) str);
 		str++;
 	}
 	if (c == '\0')
 		return ((char *) str);
 	else
-		return (NULL);
+		return (0);
 }
