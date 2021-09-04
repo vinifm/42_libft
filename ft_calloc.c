05f2c0aae3cb6d055427d1efc832b069c64083bb
Vinicius Ferreira <67122007+vinifm@users.noreply.github.com>
2021-09-04 11:38:14 -0300

    check for allocation fail

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 11:44:55 by viferrei          #+#    #+#             */
-/*   Updated: 2021/08/26 12:21:29 by viferrei         ###   ########.fr       */
+/*   Updated: 2021/09/04 11:07:35 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*ptr;
 
 	ptr = malloc(count * size);
+	if (!ptr)
+		return (0);
 	ft_memset(ptr, 0, (count * size));
 	return (ptr);
 }
